8599a8066508caa1e36ca879042db2a67a8e0ea0
MMcClure11 <mcclme02@luther.edu>
2020-10-27 13:03:55 -0500

    removes material framework for css from files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import './App.css';
 import Home from './components/Home'
-import Typography from '@material-ui/core/Typography';
-import deepOrange from '@material-ui/core/colors/deepOrange';
-
-const color = deepOrange[500];
-
 
 function App() {
   return (
     <div className="App">
-      <Typography color="textSuccess" align="center"><h1>testing color</h1></Typography>
       <Home />
     </div>
   );
