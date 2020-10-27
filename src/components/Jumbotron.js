64125d4d13b0a8f35ffb0fbf8b6ead252950b08a
jordles113 <jordan.t.romero@gmail.com>
2020-10-27 11:47:24 -0500

    troubleshooting

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -6,6 +6,7 @@ export default class Jumbotron extends Component {
         return (
             <div>
                 <JumbotronContent />
+                I AM A NEW THING
             </div>
         )
     }
