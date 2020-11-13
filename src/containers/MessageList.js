085f851cbeccf08f60eb7ddc46ede2a670f03811
jordles113 <jordan.t.romero@gmail.com>
2020-11-13 10:36:48 -0600

    wires up messageList with backend

--- src/containers/MessageList.js
+++ src/containers/MessageList.js
@@ -7,7 +7,7 @@ function MessageList(props){
   <div className='messages-div'>
     <h3> Latest Messages:</h3>
       <ul className='message-list'>
-        {props.messages.map(message => <Message text={message.text} username={message.user.username} key={message.id} />)}
+        {props.messages.map(message => <Message text={message.text} username={message.username} key={message.id} />)}
       </ul>
   </div>
   )
