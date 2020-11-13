bf6969b93f5b7d9de96574ccccfbb99f764824e0
jordles113 <jordan.t.romero@gmail.com>
2020-11-12 18:35:39 -0600

    refactors in to Sass, created variables, base, typography, utilities, header, navigation, and home

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,10 +3,10 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div className="navbar">
-      <NavLink to="/" exact className="nav-btn">Home</NavLink>
-      <NavLink to="/messages" exact className="nav-btn">Messages</NavLink>
-      <NavLink to="/fanart" exact className="nav-btn">Fan Art</NavLink>
+    <div className="navbar u-margin-bottom-small">
+      <NavLink to="/" exact className="navbar__btn">Home</NavLink>
+      <NavLink to="/messages" exact className="navbar__btn">Messages</NavLink>
+      <NavLink to="/fanart" exact className="navbar__btn">Fan Art</NavLink>
     </div>
   );
 };
--- src/components/Title.js
+++ src/components/Title.js
@@ -3,8 +3,15 @@ import React, { Component } from 'react'
 export default class Title extends Component {
     render() {
         return (
-            <div class='title-div'>
-                <h1 class='header'>Anime-Niac</h1>
+            <div className='header'>
+                <div className='header__text-box'>
+                    <h1 className='header__title'>Anime-Niac</h1>
+    
+                    <div className='heading-primary'>
+                        <span className="heading-primary--main">Moshi Moshi</span>
+                        <span className="heading-primary--sub"> Some other bs here</span>
+                    </div>
+                </div>
             </div>
         )
     }
